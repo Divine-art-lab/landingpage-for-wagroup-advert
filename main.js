41b972f2728a4be24117a887804ddd2e7a818d72
Divine-art-lab <divineij35@gmail.com>
2025-07-01 11:46:09 +0100

    update

--- main.js
+++ main.js
@@ -3,15 +3,7 @@ let btn = document.getElementById('add');
 function disp() {
   form.style.display = 'block';
 }
-window.addEventListener("onbeforeunload", () => {
-  alert('leaving?');
-});
 
-let span = document.querySelector('span');
-spanNum = Number(span);
-function add(spanNum) {
-  spanNum++;
-}
 setTimeout(add, 1000);
 console.log(typeof spanNum)
 
@@ -24,4 +16,4 @@ function popUp() {
 function popOut() {
   box.style.display = "none"
 }
-setTimeout(popUp, 2000);
+setInterval(popUp, 9000);
